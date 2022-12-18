71326f7da8ba5aab588111cfa570c7a0c68ad297
Yash <yash@YDs-MacBook-Air.local>
2022-12-18 20:54:00 +0100

    No leaks now

--- main.c
+++ main.c
@@ -6,11 +6,16 @@
 /*   By: ysingh <ysingh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 05:41:24 by ysingh            #+#    #+#             */
-/*   Updated: 2022/12/18 18:11:11 by ysingh           ###   ########.fr       */
+/*   Updated: 2022/12/18 20:51:26 by ysingh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static void	ft_leaks(void)
+{
+	system("leaks -q push_swap");
+}
 
 static void	ft_repeat_nums(t_stack *data, int argc)
 {
@@ -32,20 +37,26 @@ static void	ft_repeat_nums(t_stack *data, int argc)
 	}
 }
 
-int	main(int argc, char **argv)
+void	ft_init(t_stack *data, int argc)
 {
-	t_stack	*data;
-	int		i;
-
-	if (argc < 3)
-		ft_error();
-	data = (t_stack *)malloc(sizeof(t_stack));
 	data->stack_a = malloc(sizeof(int) * argc);
 	data->stack_b = malloc(sizeof(int) * argc);
 	data->str = ft_strdup("");
 	data->max = argc - 1;
 	data->size_a = argc - 1;
 	data->size_b = 0;
+}
+
+int	main(int argc, char **argv)
+{
+	t_stack	*data;
+	int		i;
+
+	atexit(ft_leaks);
+	if (argc < 3)
+		ft_error();
+	data = (t_stack *)malloc(sizeof(t_stack));
+	ft_init(data, argc);
 	i = 0;
 	while (i < argc - 1)
 	{
@@ -56,5 +67,6 @@ int	main(int argc, char **argv)
 	ft_normalize(data);
 	force(data);
 	ft_printf("%s\n", data->str);
-	return (0);
+	return (free(data->str), free(data->stack_b), free(data->stack_a),
+		free(data), 0);
 }
